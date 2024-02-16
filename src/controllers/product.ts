f538d60bd3a9eeafa048ec39acd2e2f9da550e32
yadavji200 <akhileshky201@gmail.com>
2024-02-16 18:14:43 +0530

    main

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,26 +1,63 @@
 import { TryCatch } from "../middlewares/error";
 import { Request } from "express";
 import { NewProductRequestBody } from "../types/types";
-import { Product } from "../models/product";
+import { Product } from "../models/product.js";
+import ErrorHandler from "../utils/utillity-class.js";
+import { rm } from "fs";
 
 
 
-export const    newProduct = TryCatch(
-async(req:Request <{},{},NewProductRequestBody>,res,next) =>{
-const {name,price,stock,category} = req.body
-const photo = req.file;
+
+  export const newProduct = TryCatch(
+   async (req: Request<{}, {}, NewProductRequestBody>, res, next) => {
+     const { name, price, stock, category } = req.body;
+      const photo = req.file;
+  
+      if (!photo) return next(new ErrorHandler("Please Add Photo", 4000));
+  
+      if (!name || !price || !stock || !category) {
+       rm(photo.path, () => {
+         console.log("Deleted");
+        });
+  
+        return next(new ErrorHandler("Please enter All Fields", 400));
+      }
+      
+  
 
 await Product.create({
     name,
     price,
     stock,
     category: category.toLocaleLowerCase(),
-    photo:photo?.path,
+    photo:photo.path,
 });
 
 return res.status(201).json({
     success: true,
     message: "product Created Successfully",
 });
+
+});
+
+
+export const getlatestProducts = TryCatch(async(req,res,next) =>{
+   const Products = await Product.find({}).sort({ createdAt: -1 }).limit(5);
+
+   return res.status(200).json({
+    success: true,
+    Products,
+   });
+
+});
+export const getAllCategories = TryCatch(async(req,res,next) =>{
+ 
+  const cateogories: any = await Product.distinct("category");
+
+
+  return res.status(200).json({
+   success: true,
+   cateogories,
+  });
 
 });
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
-import { adminOnly } from "../middlewares/auth";
-import { newProduct } from "../controllers/product";
+import { adminOnly } from "../middlewares/auth.js";
+import { getAllCategories, getlatestProducts, newProduct } from "../controllers/product.js";
 import {singleUpload } from "../middlewares/multer";
 import { error } from "console";
 import { Product } from "../models/product";
@@ -11,18 +11,18 @@ const app = express.Router();
 const deleteOneDoc = async(req:Request,res:Response,next:NextFunction) => {
     console.log("Callingg...");
     try {
-       let val = await Product.findOne({ name: "Macbook" });
+       let val = await Product.findOne({ name: "" });
        console.log(val);
-       const del = await Product.deleteOne({ _id: new mongoose.Types.ObjectId("65cde6fa1364afa994f7b738") });
+       const del = await Product.deleteOne({ _id: new mongoose.Types.ObjectId("") });
        res.status(200).json({message:"Deleted"})
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "Internal Server Error" });
     }
     }
-app.post("/new",singleUpload, newProduct)
-app.delete('/',deleteOneDoc)
-
-
+app.post("/new", adminOnly, singleUpload, newProduct);
+app.delete('/',deleteOneDoc);
+app.get("/latest", getlatestProducts);
+app.get("/cateogories", getAllCategories);
 
 export default app;
